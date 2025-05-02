e2f6bc12be2b5928c7414ee532a1d4f4930f0f58
tk <t.khamiam@gmail.com>
2025-05-02 22:09:10 +0700

    feat(news_: updated home's news section

--- src/data/misc.js
+++ src/data/misc.js
@@ -10,3 +10,35 @@ export const systemRequirements = () => {
     { hardware: "Storage", data: "20 GB available space" },
   ];
 };
+
+export const newsItems = [
+  {
+    title: 'IGF & Choice Awards',
+    image: 'https://gdconf.com/sites/default/files/IGF%20GDCA%20costream.png',
+    alt: 'IGF & Choice Awards',
+    updated: 'Last updated 3 mins ago',
+    description:
+      'The IGF competition awards winners in eight categories during GDC. Join thousands of attendees to honor innovative and independently created video games from around the world with any GDC pass before the Game Developers Choice Awards.',
+  },
+  {
+    title: 'Kick off 2025 with these game jams!',
+    image: 'https://img.itch.zone/aW1nLzE5MjY2NTI2LmpwZw==/original/jk0o3d.jpg',
+    alt: "Pizza Doggy's Ham-Jam",
+    description:
+      'the goons squad is coming. let see first 12 may 2025 rosan rdroookvkkvkaxcac kopk',
+  },
+  {
+    title: "'Consume Me' Wins Grand Prize, Nuovo Award at the 2025 IGF Awards",
+    image: 'https://gdconf.com/sites/default/files/ss_0277c27cc3a4c7ace2d33fe0c036bd35d60a1ad1.1920x1080.jpg',
+    alt: 'Consume Me',
+    description:
+      'the goons squad is coming. let see first 12 may 2025 rosan rdroookvkkvkaxcac kopk',
+  },
+  {
+    title: "Moon Studios' Ori series tops 15m sales",
+    image: 'https://assetsio.gnwcdn.com/Ori-and-the-Will-of-the-Wisps1-scaled-4035452908.jpg?width=720&quality=70&format=jpg&auto=webp',
+    alt: 'Ori series',
+    description:
+      'the goons squad is coming. let see first 12 may 2025 rosan rdroookvkkvkaxcac kopk',
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, Typography, Paper } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Paper,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+} from "@mui/material";
 import Heading from "../components/common/Heading";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import theme from "../theme";
@@ -8,6 +16,7 @@ import SwiperAutoplay from "../components/common/SwiperAutoplay";
 import SwiperGrid from "../components/common/SwiperGrid";
 
 import products from "../data/products.json";
+import { newsItems } from "../data/misc";
 
 const Home = () => {
   const recommendedGames = products.slice(0, 5);
@@ -43,6 +52,181 @@ const Home = () => {
       >
         <Paper elevation={3} />
         <Heading section="News" />
+        <Box
+          sx={{
+            backgroundColor: theme.palette.accent.emphasisdark,
+            color: theme.palette.secondary.contrastText,
+            padding: 2,
+          }}
+        >
+          <Box
+            sx={{
+              height: 24,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginX: "auto",
+            }}
+          >
+            <Box sx={{ display: "flex", gap: 4 }}>
+              <CalendarMonthIcon
+                sx={{
+                  display: { xs: "none", md: "block" },
+                }}
+              />
+              <Typography
+                variant="body1"
+                sx={{
+                  display: { xs: "none", md: "block" },
+                  textTransform: "uppercase",
+                  color: theme.palette.secondary.contrastText,
+                }}
+              >
+                28/03
+              </Typography>
+            </Box>
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: "bold",
+                whiteSpace: "nowrap",
+                color: theme.palette.secondary.contrastText,
+              }}
+            >
+              TOKYO GAME SHOW 2025 Oversea Sessions
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                whiteSpace: "nowrap",
+                textTransform: "uppercase",
+                color: theme.palette.secondary.contrastText,
+              }}
+            >
+              10am Justco Amarin Plaza
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            gap: 4,
+            alignItems: "stretch",
+            marginY: 4,
+            paddingX: 4,
+          }}
+        >
+          <Box sx={{ flex: 3, display: "flex", flexDirection: "column" }}>
+            <Card
+              sx={{
+                borderRadius: 4,
+                display: "flex",
+                flexDirection: "column",
+                flexGrow: 1,
+                backgroundColor: theme.palette.background.card,
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={newsItems[0].image}
+                alt={newsItems[0].alt}
+                sx={{ borderTopLeftRadius: 4, borderTopRightRadius: 4 }}
+              />
+              <Box
+                sx={{
+                  backgroundColor: theme.palette.background.layout,
+                  textAlign: "center",
+                  color: theme.palette.secondary.light,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  paddingY: 1,
+                }}
+              >
+                <Typography variant="caption">
+                  {newsItems[0].updated}
+                </Typography>
+              </Box>
+              <CardContent
+                sx={{
+                  p: 3,
+                  flexGrow: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography
+                  variant="body1"
+                  sx={{ color: theme.palette.secondary.light }}
+                >
+                  {newsItems[0].title}
+                </Typography>
+                <Typography variant="body2">
+                  {newsItems[0].description}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+          <Box
+            sx={{ flex: 2, display: "flex", flexDirection: "column", gap: 2 }}
+          >
+            {newsItems.slice(1).map((item, index) => (
+              <Card
+                key={index}
+                sx={{
+                  borderRadius: 4,
+                  display: "flex",
+                  flexDirection: "row",
+                  gap: 2,
+                  flexGrow: 1,
+                  backgroundColor: theme.palette.background.card,
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  image={item.image}
+                  alt={item.alt}
+                  sx={{
+                    borderRadius: 4,
+                    maxWidth: "45%",
+                    height: "auto",
+                    flexShrink: 0,
+                  }}
+                />
+                <CardContent
+                  sx={{
+                    p: 2,
+                    flexGrow: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Typography
+                    variant="body1"
+                    fontWeight="medium"
+                    sx={{
+                      color: theme.palette.secondary.light,
+                    }}
+                  >
+                    {item.title}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      display: { xs: "none", lg: "block" },
+                      color: theme.palette.primary.contrastText,
+                    }}
+                  >
+                    {item.description}
+                  </Typography>
+                </CardContent>
+              </Card>
+            ))}
+          </Box>
+        </Box>
       </Box>
     </>
   );
